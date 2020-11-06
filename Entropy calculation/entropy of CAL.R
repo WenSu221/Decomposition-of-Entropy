b64a0d60872f6f48fe3e799d2f7468aa6223e79b
Wen_S <WenSu221@users.noreply.github.com>
2020-11-06 16:47:57 +1100

    6/11/2020

--- Entropy calculation/entropy of CAL.R
+++ Entropy calculation/entropy of CAL.R
@@ -1,4 +1,4 @@
-#### figure 2 entropy of CAL ####
+#### entropy of CAL cross-national decomposition ####
 
 ### CAL function # ----
 
@@ -43,8 +43,8 @@ CALdagfunc <-function(Mx1,Y){
 
 ### Data fitting # ----
 
-A1 <- read.table("Data/GBRTENW.mltper_1x1.txt",header=TRUE,fill=TRUE,skip=1)
-A2 <- read.table("Data/FRATNP.mltper_1x1.txt",header=TRUE,fill=TRUE,skip=1)
+A1 <- read.table("Data/SWE.fltper_1x1.txt",header=TRUE,fill=TRUE,skip=1)
+A2 <- read.table("Data/SWE.mltper_1x1.txt",header=TRUE,fill=TRUE,skip=1)
 
 Y1 <- 1840
 Y2 <- 2018
@@ -56,7 +56,7 @@ qx1<-matrix(1-A1$qx,111)
 qx2<-matrix(1-A2$qx,111)
 
 
-## CAL #
+## CAL # ----
 
 CAL1 <- c()
 for (i in seq(1952,2017,5)){
@@ -68,7 +68,7 @@ for (i in seq(1952,2017,5)){
   CAL2 <- c(CAL2, CALfunc(qx2,i))
 }
 
-## CAL dagger #
+## CAL dagger # ----
 
 qx1 <- ifelse(qx1==0,1,qx1)
 qx2 <- ifelse(qx2==0,1,qx2)
@@ -86,8 +86,12 @@ for (i in seq(1952,2017,5)){
 CALdagger1 <- CALdagger1*-1
 CALdagger2 <- CALdagger2*-1
 
+## entropy # ----
+
 entropyCAL1 <- CALdagger1/CAL1
 entropyCAL2 <- CALdagger2/CAL2
+
+## decomp # ----
 
 entropyavg <- (entropyCAL1+entropyCAL2)/2
 entropydiff <- entropyCAL1-entropyCAL2
@@ -98,3 +102,6 @@ measure <- log(CAL1/CAL2)
 right <- (dispersion - measure)*entropyavg
 left <- entropydiff
 round(right-left, 5)
+
+inequality <- dispersion*-1/(measure+dispersion*-1)
+longevity <- measure/(measure+dispersion*-1)
