bbdbcd24857f693b61e566d6093236b5b7672570
Wen_S <WenSu221@users.noreply.github.com>
2021-01-18 09:32:06 +1100

    18/1/2021

--- Figures/figure 2 - countries compare male.R
+++ Figures/figure 2 - countries compare male.R
@@ -58,6 +58,7 @@ A6 <- read.table("Data/FIN.mltper_1x1.txt",header=TRUE,fill=TRUE,skip=1)
 A7 <- read.table("Data/ITA.mltper_1x1.txt",header=TRUE,fill=TRUE,skip=1)
 A8 <- read.table("Data/GBRSCO.mltper_1x1.txt",header=TRUE,fill=TRUE,skip=1)
 A9 <- read.table("Data/NLD.mltper_1x1.txt",header=TRUE,fill=TRUE,skip=1)
+A10 <- read.table("Data/CHE.mlper_1x1.txt",heaser=TRUE,fill=TRUE,skip=1)
 
 Y1 <- 1877
 Y2 <- 2017
@@ -71,6 +72,7 @@ A6<-A6[(A6$Year>Y1)&(A6$Year<(Y2+1)),]
 A7<-A7[(A7$Year>Y1)&(A7$Year<(Y2+1)),]
 A8<-A8[(A8$Year>Y1)&(A8$Year<(Y2+1)),]
 A9<-A9[(A9$Year>Y1)&(A9$Year<(Y2+1)),]
+A10<-A10[(A10$Year>Y1)&(A10$Year<(Y2+1)),]
 
 qx1<-matrix(1-A1$qx,111)
 qx2<-matrix(1-A2$qx,111)
@@ -81,53 +83,60 @@ qx6<-matrix(1-A6$qx,111)
 qx7<-matrix(1-A7$qx,111)
 qx8<-matrix(1-A8$qx,111)
 qx9<-matrix(1-A9$qx,111)
+qx10<-matrix(1-A10$qx,111)
 
 ## CAL ####
 
 CAL1 <- c()
-for (i in seq(1989,2017,4)){
+for (i in seq(1989,2017,2)){
   CAL1 <- c(CAL1, CALfunc(qx1,i))
 }
 
 CAL2 <- c()
-for (i in seq(1989,2017,4)){
+for (i in seq(1989,2017,2)){
   CAL2 <- c(CAL2, CALfunc(qx2,i))
 }
 
 CAL3 <- c()
-for (i in seq(1989,2017,4)){
+for (i in seq(1989,2017,2)){
   CAL3 <- c(CAL3, CALfunc(qx3,i))
 }
 
 CAL4 <- c()
-for (i in seq(1989,2017,4)){
+for (i in seq(1989,2017,2)){
   CAL4 <- c(CAL4, CALfunc(qx4,i))
 }
 
 CAL5 <- c()
-for (i in seq(1989,2017,4)){
+for (i in seq(1989,2017,2)){
   CAL5 <- c(CAL5, CALfunc(qx5,i))
 }
 
 CAL6 <- c()
-for (i in seq(1989,2017,4)){
+for (i in seq(1989,2017,2)){
   CAL6 <- c(CAL6, CALfunc(qx6,i))
 }
 
 CAL7 <- c()
-for (i in seq(1989,2017,4)){
+for (i in seq(1989,2017,2)){
   CAL7 <- c(CAL7, CALfunc(qx7,i))
 }
 
 CAL8 <- c()
-for (i in seq(1989,2017,4)){
+for (i in seq(1989,2017,2)){
   CAL8 <- c(CAL8, CALfunc(qx8,i))
 }
 
 CAL9 <- c()
-for (i in seq(1989,2017,4)){
+for (i in seq(1989,2017,2)){
   CAL9 <- c(CAL9, CALfunc(qx9,i))
 }
+
+CAL10 <- c()
+for (i in seq(1989,2017,2)){
+  CAL10 <- c(CAL10, CALfunc(qx10,i))
+}
+
 ## CAL dagger ####
 
 qx1 <- ifelse(qx1==0,1,qx1)
@@ -139,50 +148,56 @@ qx6 <- ifelse(qx6==0,1,qx6)
 qx7 <- ifelse(qx7==0,1,qx7)
 qx8 <- ifelse(qx8==0,1,qx8)
 qx9 <- ifelse(qx9==0,1,qx9)
+qx10 <- ifelse(qx10==0,1,qx10)
 
 CALdagger1 <- c()
-for (i in seq(1989,2017,4)){
+for (i in seq(1989,2017,2)){
   CALdagger1 <- c(CALdagger1, CALdagfunc(qx1,i))
 }
 
 CALdagger2 <- c()
-for (i in seq(1989,2017,4)){
+for (i in seq(1989,2017,2)){
   CALdagger2 <- c(CALdagger2, CALdagfunc(qx2,i))
 }
 
 CALdagger3 <- c()
-for (i in seq(1989,2017,4)){
+for (i in seq(1989,2017,2)){
   CALdagger3 <- c(CALdagger3, CALdagfunc(qx3,i))
 }
 
 CALdagger4 <- c()
-for (i in seq(1989,2017,4)){
+for (i in seq(1989,2017,2)){
   CALdagger4 <- c(CALdagger4, CALdagfunc(qx4,i))
 }
 
 CALdagger5 <- c()
-for (i in seq(1989,2017,4)){
+for (i in seq(1989,2017,2)){
   CALdagger5 <- c(CALdagger5, CALdagfunc(qx5,i))
 }
 
 CALdagger6 <- c()
-for (i in seq(1989,2017,4)){
+for (i in seq(1989,2017,2)){
   CALdagger6 <- c(CALdagger6, CALdagfunc(qx6,i))
 }
 
 CALdagger7 <- c()
-for (i in seq(1989,2017,4)){
+for (i in seq(1989,2017,2)){
   CALdagger7 <- c(CALdagger7, CALdagfunc(qx7,i))
 }
 
 CALdagger8 <- c()
-for (i in seq(1989,2017,4)){
+for (i in seq(1989,2017,2)){
   CALdagger8 <- c(CALdagger8, CALdagfunc(qx8,i))
 }
 
 CALdagger9 <- c()
-for (i in seq(1989,2017,4)){
+for (i in seq(1989,2017,2)){
   CALdagger9 <- c(CALdagger9, CALdagfunc(qx9,i))
+}
+
+CALdagger10 <- c()
+for (i in seq(1989,2017,2)){
+  CALdagger10 <- c(CALdagger10, CALdagfunc(qx10,i))
 }
 
 CALdagger1 <- CALdagger1*-1
@@ -194,6 +209,7 @@ CALdagger6 <- CALdagger6*-1
 CALdagger7 <- CALdagger7*-1
 CALdagger8 <- CALdagger8*-1
 CALdagger9 <- CALdagger9*-1
+CALdagger10 <- CALdagger10*-1
 
 ## entropy ####
 
@@ -206,37 +222,15 @@ entropyCAL6 <- CALdagger6/CAL6
 entropyCAL7 <- CALdagger7/CAL7
 entropyCAL8 <- CALdagger8/CAL8
 entropyCAL9 <- CALdagger9/CAL9
+entropyCAL10 <- CAldagger10/CAL10
 
 CALavg <- rbind(entropyCAL1,entropyCAL2,entropyCAL3,
                 entropyCAL4,entropyCAL5,entropyCAL6,
-                entropyCAL7,entropyCAL8,entropyCAL9)
+                entropyCAL7,entropyCAL8,entropyCAL9,
+                entropyCAL10)
 CALavg <- colMeans(CALavg)
 
 
-#### Figure ####
-
-Years <- seq(1989,2017,4)
-png(file = "Output/Countries Comparison log scale, male 1989-2017.png",
-    units = "in", width = 6, height = 8, res = 200)
-plot(c(1989,2017),rev(c(0.11,0.24)),col = 0,
-     xlab = "Years",ylab = "entropy of CAL (log scale)", 
-     log = "y")
-lines(Years,entropyCAL1,col = cols[1],lty=5,lwd = 2)
-lines(Years,entropyCAL2,col = cols[2],pch=2,lty=5,lwd = 2)
-lines(Years,entropyCAL3,col = cols[3],lty=5,lwd = 2)
-lines(Years,entropyCAL4,col = cols[4],pch=3,lty=5,lwd = 2)
-lines(Years,entropyCAL5,col = cols[5],pch=2,lty=5,lwd = 2)
-lines(Years,entropyCAL6,col = cols[6],lty=6,lwd = 2)
-lines(Years,entropyCAL7,col = cols[7],pch=3,lty=6,lwd = 2)
-lines(Years,entropyCAL8,col = cols[8],lty=6,lwd = 2)
-lines(Years,entropyCAL9,col = cols[9],lty=6,lwd = 2)
-lines(Years,CALavg,col = "grey",pch = 16,lty = 1,lwd = 2)
-title("comparison of entropy of CAL across countries, male 1989-2017")
-legend("topright",c("Sweden","Denmark","France","England and Wales",
-                    "Norway","Finland","Italy","Scotland","Netherland","average"),
-       col = c(cols[1:9]),lty = c(5,5,5,5,5,6,6,6,6,1),
-       box.col = 0)
-dev.off()
 
 ### CALavg as the benchmark ####
 
@@ -254,7 +248,7 @@ diff10 <- entropyCAL10 - CALavg
 
 ### Plots ####
 
-Years <- seq(1989,2017,4)
+Years <- seq(1989,2017,2)
 png(file = "Output/Countries Comparison average benchmark, male 1989-2017.png",
     units = "in", width = 7, height = 9, res = 300)
 plot(c(1989,2017),c(-0.025,0.04),col = 0,
@@ -290,7 +284,7 @@ lines(Years,diff9,type = "l",
 lines(Years,diff10,type = "l",
       col = cols[10],lty=2,lwd = 2,
       cex = 1,pch = 20)
-lines(Years,rep(0,times=8),
+lines(Years,rep(0,times=15),
       col = 1, lty = 3, lwd = 2,
       cex = 1,pch = 20)
 legend("topright",c("Sweden","Denmark","France",
