e6d9395fde208d42e510c7002a8d2b0ac78504ed
Wen_S <WenSu221@users.noreply.github.com>
2021-05-02 17:10:27 +1000

    2/5/2021

--- main/figure A - standardized entropy.R
+++ main/figure A - standardized entropy.R
@@ -1,6 +1,8 @@
 #### entropy as a standardized measure of inequality in mortality ####
 
 library(dplyr)
+library(ggplot2)
+library(metR)
 
 ### get mortality data from HMD ####
 # by Pascariu
@@ -26,7 +28,7 @@ table_final2 <- c()
 table_total <- c()
 average_final <- c()
 
-for (x in 1950:2017){
+for (x in 1957:2017){
   year <- x
   test <- filter(dta,dta[,2]==year)
   
@@ -71,53 +73,89 @@ row.names(table_total) <- c(1:length(table_total$edagger))
 row.names(table_final1) <- c(1:length(table_final1$edagger))
 row.names(table_final2) <- c(1:length(table_final2$edagger))
 
-duplicate.no <- duplicated(c(table_total$entropy,table_final1$entropy,table_final2$entropy))
-table_total <- table_total[!duplicate.no,]
+# duplicate.no <- duplicated(c(table_total$entropy,table_final1$entropy,table_final2$entropy))
+# table_total <- table_total[!duplicate.no,]
+# table_total <- na.omit(table_total)
 ### Plot ####
 
-myrange1 <- range(table_final1$entropy)
-myrange2 <- range(table_final1$edagger)
-myyears <- c(1950,2017)
-
-plot(myrange2,myrange1,col = 0)
-points(table_final1$edagger,table_final1$entropy,col= "darkblue")
-lines(average_final[,2],average_final[,3])
-
-myrange3 <- range(table_final2$entropy)
-myrange4 <- range(table_final2$edagger)
-
-plot(myrange4,myrange3,col = 0)
-points(table_final2$edagger,table_final2$entropy,col= "darkblue")
-lines(average_final[,2],average_final[,3])
-
-myrange5 <- range(table_total$entropy)
-myrange6 <- range(table_total$edagger)
-points(table_total$edagger,table_total$entropy,col= "darkgrey")
-lines(average_final[,2],average_final[,3], col = "red", lwd = 2)
-points(table_final2$edagger,table_final2$entropy,col= "darkgreen", pch = 7)
-points(table_final1$edagger,table_final1$entropy,col= "darkblue")
+# myrange1 <- range(table_final1$entropy)
+# myrange2 <- range(table_final1$edagger)
+# myyears <- c(1950,2017)
+# 
+# plot(myrange2,myrange1,col = 0)
+# points(table_final1$edagger,table_final1$entropy,col= "darkblue")
+# lines(average_final[,2],average_final[,3])
+# 
+# myrange3 <- range(table_final2$entropy)
+# myrange4 <- range(table_final2$edagger)
+# 
+# plot(myrange4,myrange3,col = 0)
+# points(table_final2$edagger,table_final2$entropy,col= "darkblue")
+# lines(average_final[,2],average_final[,3])
+# 
+# myrange5 <- range(table_total$entropy)
+# myrange6 <- range(table_total$edagger)
+# points(table_total$edagger,table_total$entropy,col= "darkgrey")
+# lines(average_final[,2],average_final[,3], col = "red", lwd = 2)
+# points(table_final2$edagger,table_final2$entropy,col= "darkgreen", pch = 7)
+# points(table_final1$edagger,table_final1$entropy,col= "darkblue")
 
 ### ggplot2
-library(ggplot2)
 
-table_total <- as.matrix(table_total)
-table_final1 <- as.matrix(table_final1)
-table_final2 <- as.matrix(table_final2)
+# 
+# table_total <- as.matrix(table_total)
+# table_final1 <- as.matrix(table_final1)
+# table_final2 <- as.matrix(table_final2)
+# 
+# plotdata <- data.frame(
+#   c(table_total[,2],table_final1[,2],table_final2[,2]),
+#   c(rep("salv entropy",length(table_total[,4])),
+#     rep("hta entropy",183),rep("lta entrpy",135)),
+#   c(table_total[,4],table_final1[,4],table_final2[,4]),
+#   c(table_total[,5],table_final1[,5],table_final2[,5])
+# )
+# 
+# colnames(plotdata) <- c("years","type","lifespan_inequality","entropy")
+# 
+# plotdata$type <- factor(plotdata$type)
+# plotdata$years <- as.numeric(plotdata$years)
 
-plotdata <- data.frame(
-  c(table_total[,2],table_final1[,2],table_final2[,2]),
-  c(rep("salv entropy",length(table_total[,4])),
-    rep("hta entropy",183),rep("lta entrpy",135)),
-  c(table_total[,4],table_final1[,4],table_final2[,4]),
-  c(table_total[,5],table_final1[,5],table_final2[,5])
+# ggplot(data = plotdata,aes(lifespan_inequality,entropy,alpha = years,color = type))+
+#   geom_point()+
+#   geom_line(average_final,mapping = aes(edagger_mean,entropy_mean),alpha = year,color= "black")+
+#   scale_color_manual(values = c("red","blue","grey"))
+
+### contour
+
+le.range <- round(range(table_total$ezero),1)
+lsv.range <- round(range(table_total$edagger),1)
+
+le.seq <- seq(le.range[1],le.range[2],0.1)
+lsv.seq <- seq(lsv.range[1],lsv.range[2],0.1)
+
+entropy.seq <- c()
+
+for (i in 1:length(lsv.seq)){
+  hx <- lsv.seq[i]/le.seq
+  entropy.seq <- c(entropy.seq,hx)
+}
+
+data.seq <- data.frame(
+  rep(lsv.seq,each = length(le.seq)),
+  rep(le.seq,times = length(lsv.seq)),
+  entropy.seq
 )
 
-colnames(plotdata) <- c("years","type","lifespan_inequality","entropy")
+colnames(data.seq) <- c("edagger","life.expectancy","entropy")
 
-plotdata$type <- factor(plotdata$type)
-plotdata$years <- as.numeric(plotdata$years)
-
-ggplot(data = plotdata,aes(lifespan_inequality,entropy,alpha = years,color = type))+
-  geom_point()+
-  geom_line(average_final,mapping = aes(edagger_mean,entropy_mean),alpha = year,color= "black")
-  scale_color_manual(values = c("red","blue","grey"))
+ggplot(data = data.seq,aes(x = edagger,y = life.expectancy))+
+  geom_raster(aes(fill = entropy))+
+  geom_contour(aes(z = entropy),color = "black",alpha = 0.5)+
+  geom_point(data = table_total,aes(x = edagger,y = ezero,alpha = time),color = "darkseagreen")+
+  geom_text_contour(aes(z = entropy),color = "black")+
+  scale_fill_continuous(low = "yellow", high = "red")+
+  scale_alpha_continuous(range = rev(c(0.1,0.6)))+
+  labs(x = "lifespan inequality", y = "life expectancy")+
+  scale_y_continuous(expand = c(0,0))+
+  scale_x_continuous(breaks = scales::breaks_width(1), expand = c(0,0))
+  
